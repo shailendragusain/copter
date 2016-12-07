7c73dc1294c4ca137e8a28918aa6e608b3aee8b5
shailendragusain <shail9689@gmail.com>
2016-12-08 03:53:13 +0530

    Setup jQuery

--- app/js/copter.js
+++ app/js/copter.js
@@ -1,15 +1,15 @@
 (function($) {
-	var startAnimation = function(el){
-		console.log("animate");
-		console.log(el);
+	var isRunning = false, steps = [];
+	var start = function(el){
+		isRunning = true;
 	};
 
 	$.fn.copter = function(options){
 		var settings = $.extend({
-			
+
 		}, options);
 
-		if(!animating)
-			startAnimation(this);
+		if(!isRunning)
+			start(this);
 	};
 }(jQuery));
